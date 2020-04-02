08fe9914e2bf09169ae39d3ec0e2aa054c80ea3e
Senju14589 <59078952+Senju14589@users.noreply.github.com>
2020-04-02 17:02:04 +0700

    project_623410008-8

---
+++ project4/src/main/java/com/mycompany/project4/Box1.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project4;
+
+/**
+ *
+ * @author comzone
+ */
+public class Box1 {
+  public static void main(String args[]) {
+         String[] ListProvince = new String[] { "Bangkok",
+ "Pattaya",
+ "Krabi",
+ "Kanchanaburi",
+ "Kalasin",
+ "Kamphaeng Phet",
+ "Khon Kaen",
+ "Chanthaburi",
+ "Chachoengsao",
+ "Chon Buri",
+ "Chai Nat",
+ "Chaiyaphum",
+ "Chumphon",
+ "Trang",
+ "Trat",
+ "Tak",
+ "Nakhon Nayok",
+ "Nakhon Pathom",
+ "Nakhon Phanom",
+ "Nakhon Ratchasima",
+ "Nakhon Si Thammarat",
+ "Nakhon Sawan",
+ "Nonthaburi",
+ "Narathiwat",
+ "Nan",
+ "Bueng Kan",
+ "Buri Ram",
+ "Pathum Thani",
+ "Prachuap Khiri Khan",
+ "Prachin Buri",
+ "Pattani",
+ "Phra Nakhon Si Ayutthaya",
+ "Phayao",
+ "Phangnga",
+ "Phatthalung",
+ "Phichit",
+ "Phitsanulok",
+ "Phuket",
+ "Maha Sarakham",
+ "Mukdahan",
+ "Yala",
+ "Yasothon",
+ "Ranong",
+ "Rayong",
+ "Ratchaburi",
+ "Roi Et",
+ "Lop Buri",
+ "Lampang",
+ "Lamphun",
+ "Si Sa Ket",
+ "Sakon Nakhon",
+ "Songkhla",
+ "Satun",
+ "Samut Prakan",
+ "Samut Songkhram",
+ "Samut Sakhon",
+ "Saraburi",
+ "Sa Kaeo",
+ "Sing Buri",
+ "Suphan Buri",
+ "Surat Thani",
+ "Surin",
+ "Sukhothai",
+ "Nong Khai",
+ " Nong Bua Lam Phu",
+ " Amnat Charoen",
+ "Udon Thani",
+ " Uttaradit",
+ " Uthai Thani",
+ " Ubon Ratchathani",
+ " Ang Thong",
+ " Chiang Rai",
+ " Chiang Mai",
+"Phetchaburi",
+" Phetchabun",
+"Loei",
+"Phrae",
+"Mae Hong Son"};
+         Form1 f = new Form1(ListProvince);
+         f.show();
+     }
+        
+        }
+
